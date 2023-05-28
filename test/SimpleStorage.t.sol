193f74bcf6a7bbbec6e1837e1899025d0463af34
alymurtazamemon <alimurtaza-assistant@outlook.com>
2023-05-28 14:54:44 +0500

    change Test contract name

--- test/SimpleStorage.t.sol
+++ test/SimpleStorage.t.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.13;
 import "forge-std/Test.sol";
 import "../src/SimpleStorage.sol";
 
-contract CounterTest is Test {
+contract SimpleStorageTest is Test {
     SimpleStorage public simpleStorage;
 
     function setUp() external {
